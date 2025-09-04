19edb0db4b8a2cad3878941bd116fd705b2140f4
Luis-Gil2 <l3877113@gmail.com>
2025-09-04 14:39:02 -0600

    Add files via upload

---
+++ ADA_2_LUIS_GIL_3sa.py
@@ -0,0 +1,13 @@
+frutas=[]
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")
+frutas.append("Uva")
+
+print(frutas)
+
+frutas.pop(0) # Elimina el primero de la lista 
+frutas.pop(1) #Elimina el que esta en la posicion 1 
+
+frutas.append("Sandias")
+print(frutas)
